277273ca76a3bbbef866db4e85934248c90806ba
BENDENG1 <76191161+BENDENG1@users.noreply.github.com>
2024-06-09 22:25:57 +0900

    chore : update build.gradles

--- gyunuk/composestudy/app/build.gradle.kts
+++ gyunuk/composestudy/app/build.gradle.kts
@@ -1,6 +1,10 @@
+import com.android.build.gradle.internal.cxx.configure.gradleLocalProperties
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
+    id("com.google.dagger.hilt.android")
 }
 
 android {
@@ -18,6 +22,7 @@ android {
         vectorDrawables {
             useSupportLibrary = true
         }
+        buildConfigField("String", "KAKAO_TOKEN", getProperty("kakaotoken"))
     }
 
     buildTypes {
@@ -30,14 +35,15 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
     buildFeatures {
         compose = true
+        buildConfig = true
     }
     composeOptions {
         kotlinCompilerExtensionVersion = "1.4.3"
@@ -49,11 +55,17 @@ android {
     }
 }
 
+fun getProperty(propertyKey: String): String {
+    return gradleLocalProperties(rootDir).getProperty(propertyKey)
+}
+
 dependencies {
     val nav_version = "2.7.7"
     val orbit_version = "8.0.0"
     val kakao_version = "2.20.1"
     val glide_version = "1.3.7"
+    val retrofitVersion = "2.9.0"
+    val okHttpVersion = "5.0.0-alpha.2"
 
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.0")
@@ -76,6 +88,7 @@ dependencies {
 
     //jetpack navigation
     implementation("androidx.navigation:navigation-compose:$nav_version")
+    implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
 
     //orbit
     implementation("org.orbit-mvi:orbit-core:$orbit_version")
@@ -88,4 +101,17 @@ dependencies {
 
     //glide
     implementation ("com.github.skydoves:landscapist-glide:$glide_version")
+
+    //hilt
+    implementation("com.google.dagger:hilt-android:2.48")
+    kapt("com.google.dagger:hilt-android-compiler:2.48")
+
+    // retrofit
+    api("com.squareup.retrofit2:retrofit:$retrofitVersion")
+    api("com.squareup.retrofit2:converter-gson:$retrofitVersion")
+
+    // okHttp
+    api("com.squareup.okhttp3:okhttp:$okHttpVersion")
+    api("com.squareup.okhttp3:logging-interceptor:$okHttpVersion")
+    api("com.squareup.okhttp3:okhttp-urlconnection:$okHttpVersion")
 }
